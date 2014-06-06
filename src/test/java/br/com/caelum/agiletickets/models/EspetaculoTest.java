db5b0fb0cd6d315babe8da2f9d9875eeca136900
herbert trindade <herbert.curso@caelum.com.br>
2014-06-05 21:49:52 -0300

    metodo criasessoes

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -1,8 +1,14 @@
 package br.com.caelum.agiletickets.models;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.junit.Test;
 
 public class EspetaculoTest {
@@ -81,4 +87,53 @@ public class EspetaculoTest {
 		return sessao;
 	}
 	
+	@Test
+	public void deveCriarSessoesDiarias()
+	{
+		Espetaculo espetaculo = new Espetaculo();
+		
+		List<Sessao> list = espetaculo.criaSessoes(LocalDate.now(), LocalDate.now().plusDays(4), LocalTime.now(), Periodicidade.DIARIA);
+		
+		assertEquals(4, list.size());
+	}
+	
+	@Test
+	public void deveCriarSessoesDiariasMesmoDia()
+	{
+		Espetaculo espetaculo = new Espetaculo();
+		
+		List<Sessao> list = espetaculo.criaSessoes(LocalDate.now(), LocalDate.now(), LocalTime.now(), Periodicidade.DIARIA);
+		
+		assertEquals(1, list.size());
+	}
+	
+	@Test
+	public void deveCriarSessoesInicialMenorQueFinal()
+	{
+		Espetaculo espetaculo = new Espetaculo();
+		
+		List<Sessao> list = espetaculo.criaSessoes(LocalDate.now().plusDays(8), LocalDate.now(), LocalTime.now(), Periodicidade.DIARIA);
+		
+		assertEquals(0, list.size());
+	}
+	
+	@Test
+	public void deveCriarSessoesSemanaisMesmoDia()
+	{
+		Espetaculo espetaculo = new Espetaculo();
+		
+		List<Sessao> list = espetaculo.criaSessoes(LocalDate.now(), LocalDate.now(), LocalTime.now(), Periodicidade.SEMANAL);
+		
+		assertEquals(1, list.size());
+	}
+	
+	@Test
+	public void deveCriar2SessoesSemanais()
+	{
+		Espetaculo espetaculo = new Espetaculo();
+		
+		List<Sessao> list = espetaculo.criaSessoes(LocalDate.now(), LocalDate.now().plusDays(8), LocalTime.now(), Periodicidade.SEMANAL);
+		
+		assertEquals(2, list.size());
+	}
 }
